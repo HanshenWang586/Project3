1d02836e2144d8527f95dfe9d71fc1d8dd29f4b7
HanshenWang586 <hanshen.wang@outlook.com>
2016-11-10 15:09:46 -0500

    eleventh commit

--- HW3-StudentCopy/twitterhw3b.py
+++ HW3-StudentCopy/twitterhw3b.py
@@ -7,6 +7,33 @@
 
 # Be prepared to change the search term during demo.
 
+import tweepy
+from textblob import TextBlob
 
-print("Average subjectivity is")
-print("Average polarity is")
+# Unique code from Twitter
+access_token = "2473973030-PJ1R1wixU6OThS8sKZVPkXHK4v6o5KfndcFDsrj"
+access_token_secret = "DM6VNbxN4XtvWPWWRJxVuHKsXLIvBirixSufWyz8OXbUE"
+consumer_key = "ZPVnQkA3plpVnX8FtRrYvMem7"
+consumer_secret = "xFkUw83rUbYWrT2Og6s68f5orOc0JcnpTDbRc6DGi9urbjbZVf"
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+
+count = 0
+sumSubj = 0
+sumPloar = 0
+tweets = api.search("Trump")
+for tweet in tweets:
+	print(tweet.text)
+	analysis = TextBlob(tweet.text)
+	count += 1
+	sumSubj += analysis.subjectivity
+	sumPolar = analysis.polarity
+	
+aveSub = sumSubj/count
+avePolar = sumPolar/count
+print("Average subjectivity is" + str(aveSub))
+print("Average polarity is" + str(avePolar))
